da383e1822910d7c85ec5ff82f13c2ca7dbec7f8
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2023-12-12 17:51:14 +0100

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
